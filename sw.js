62a5173a2d40c5f1adc07bd6c47be742022fec88
trangyk07c-spec <trangyk07c@gmail.com>
2026-08-18 21:56:53 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "laokhoa-deep-learning-2026-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => {
+    const network = fetch(event.request).then(response => {
+      if (response && response.status === 200) {
+        const clone=response.clone();
+        caches.open(CACHE_NAME).then(cache=>cache.put(event.request,clone));
+      }
+      return response;
+    }).catch(()=>cached || caches.match("./index.html"));
+    return cached || network;
+  }));
+});
